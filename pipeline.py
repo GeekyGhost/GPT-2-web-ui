8566601b0aaf65bfcdb2a28ffd508fc99156c293
GeekyGhost <111990299+GeekyGhost@users.noreply.github.com>
2023-03-19 00:50:51 -0400

    Delete pipeline.py

--- pipeline.py
+++
@@ -1,7 +0,0 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, pipeline
-
-model_name = "gpt2"
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-model = GPT2LMHeadModel.from_pretrained(model_name)
-
-generator = pipeline("text-generation", model=model, tokenizer=tokenizer, device=0)  # device=0 means using your GPU
