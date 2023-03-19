4b411f6dc473b7227ad9f79966cb5bddbe54c763
GeekyGhost <111990299+GeekyGhost@users.noreply.github.com>
2023-03-19 00:50:28 -0400

    Delete gpt2-tokenizer.py

--- gpt2-tokenizer.py
+++
@@ -1,8 +0,0 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-
-# Smallest base model of GPT-2
-model_name = "gpt2"
-
-# Download the tokenizer and the model
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-model = GPT2LMHeadModel.from_pretrained(model_name)
